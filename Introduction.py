111967e07f538e915940c1d93b5c73f45d4d5de4
Amanda Maulana <85415488+iammaul@users.noreply.github.com>
2021-11-09 23:26:35 +0700

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,7 +1,36 @@
-def namaku(kata):
-  nama = (a, b, c, d, e,)
-  print(nama)
-  return nama
+def border():
+    print('-'*25)
 
-daftar = ('Muhammad', 'Amanda', 'Maulana', 'Malik', 'Ibrahim')
-print(namaku)
+def name():
+    name = ['Muhammad',2,'Amanda',False,98,'Maulana','Malik','Ibrahim']
+    name.remove(2), name.remove(False), name.remove(98)
+    print('Halo, ')
+    print('Nama : ', name)
+
+def age():
+    import datetime as dt
+    
+    date = 17
+    month = 12
+    year = 2002
+    
+    time = dt.date(year, month, date)
+    print(f'Saya lahir tanggal {date}')
+    print('Bulan ',month)
+    print('Tahun ',year)
+        
+    now = dt.date.today()
+    print('Hari ini tanggal ',now)
+    days_age = now - time
+    year_age = days_age.days // 365
+    print('Sekarang saya berumur ',year_age)
+
+
+print("="*20+' INTRODUCTION '+'='*20)
+name()
+border()
+age()
+border()
+team = 'MARGILIM' [:: -1]
+print('Saya dari kelompok '+team)
+
